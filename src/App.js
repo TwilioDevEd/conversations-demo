87cfcc54f9b029bb2bbef9401414cb9ca9fac05d
Andres Jaan Tack <ajtack@twilio.com>
2018-10-18 08:36:28 -0700

    Added a link to the phone number (click the h1).

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import ChatApp from './ChatApp';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { numberInstructionClass: 'hidden'}
+  }
+
   render() {
     return (
       <div className="App">
         <header>
-          <h1>Twilio Messaging Sessions!</h1>
+          <h1 onClick={() => this.setState(() => ({numberInstructionClass: ''}))}>Omnichannel Group Chat</h1>
+          <h2 className={this.state.numberInstructionClass}>Send a text to +1 (415) 853-1089</h2>
         </header>
         <ChatApp />
       </div>

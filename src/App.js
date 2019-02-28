a05a9fbe747e3464ffa2f50944e97b7b1b9f940c
Andres Jaan Tack <ajtack@twilio.com>
2019-02-28 14:06:18 +0200

    Updated the readme to reflect this app's actual purpose.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { numberInstructionClass: 'hidden'}
+    this.state = {}
   }
 
   render() {
     return (
       <div className="App">
         <header>
-          <h1 onClick={() => this.setState(() => ({numberInstructionClass: ''}))}>Omnichannel Group Chat</h1>
-          <h2 className={this.state.numberInstructionClass}>Send a text to +1 (415) 853-1089</h2>
+          <h1>The Sessions Chat Demo</h1>
         </header>
         <ChatApp />
       </div>
